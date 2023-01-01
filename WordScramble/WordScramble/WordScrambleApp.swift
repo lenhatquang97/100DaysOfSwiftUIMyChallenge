a07e848d94c43da57a47d695073f152f73285a2e
p0tluck <lenhatquang97@gmail.com>
2023-01-01 12:53:55 +0700

    Add WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by LAP13526 on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
